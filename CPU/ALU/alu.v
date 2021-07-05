72fb0323ab36e42249e8ebc512cc11d7157eb952
Vindula Rathnayake <72916960+VindulaNR@users.noreply.github.com>
2021-07-05 19:17:14 +0530

    ALU updated

--- CPU/ALU/alu.v
+++ CPU/ALU/alu.v
@@ -62,56 +62,69 @@ module alu(DATA1, DATA2, RESULT,SELECT);
     	case(SELECT)
 
     	//0. ADD operation 
-    	5'b00000: 
-        	 #1 RESULT = RES_ADD; 
-    	//1. SUB operation
-    	5'b00001: 
-        	#1 RESULT = RES_SUB;
-    	//2
-    	5'b00010: 
-        	RESULT = DATA1 + DATA2;
-    	//3
-    	5'b00011: 
-        	RESULT = DATA1 ^ DATA2;
-    	//4 
-    	5'b00100: 
-        	RESULT = DATA1 << DATA2; 
-    	//5
-    	5'b00101: 
-        	RESULT = DATA1 >> DATA2;
-    	//6
-    	5'b00110: 
-        	RESULT = DATA1 - DATA2; 
-    	//7
-    	5'b00111:
-        	RESULT = DATA1 <<< DATA2;
-    	//8
-    	5'b01000:
-        	RESULT = DATA1 * DATA2;
-	//9
-	5'b01001:
-		RESULT = DATA1/DATA2;
-	//10
-	5'b01010:
-		RESULT=($signed(DATA1) < $signed(DATA2)) ? 1'b1 : 1'b0;
-	//11
-	5'b01011:
-		RESULT = 0;
-	//12
-	5'b01100:
-		RESULT = 0;
-	//13
-	5'b01101:
-		RESULT = 0;
-	//14
-	5'b01110:
-		RESULT = 0;
-	//15
-	5'b01111:
-		RESULT = 0;
-	//16
-	5'b11111:
-		RESULT = 0;
+    	5'b00000:  #1 RESULT = RES_ADD; 
+    	//1. SLL operation - Shift Left Logic
+    	5'b00001:  RESULT = RES_SLL;
+    	//2. set les than
+    	5'b00010:  RESULT = RES_SLT;
+    	//3. set less than unsigned
+    	5'b00011:  RESULT = RES_SLTU;
+    	//4. XOR
+    	5'b00100: #1 RESULT = RES_XOR; 
+    	//5. Shift Right Logic
+    	5'b00101: RESULT = RES_SRL;
+    	//6. OR
+    	5'b00110: #1 RESULT = RES_OR; 
+    	//7. AND
+    	5'b00111: #1 RESULT = RES_AND;
+    	//8. 
+    	5'b01000: RESULT = 0;
+		//9
+		5'b01001: RESULT = 0;
+		//10
+		5'b01010: RESULT = 0;
+		//11
+		5'b01011: RESULT = 0;
+		//12
+		5'b01100: RESULT = 0;
+		//13. 
+		5'b01101: RESULT = 0;
+		//14
+		5'b01110: RESULT = 0;
+		//15
+		5'b01111: RESULT = 0;
+		//16. Subtract
+		5'b10000: #1 RESULT = RES_SUB;
+		//17. 
+		5'b10001: RESULT = 0;
+		//18. 
+		5'b10010: RESULT = 0;
+		//19. 
+		5'b10011: RESULT = 0;
+		//20. 
+		5'b10100: RESULT = 0;
+		//21. SRA = Shift right arithmaic
+		5'b10101: RESULT = RES_SRA;
+		//22. 
+		5'b10110: RESULT = 0;
+		//23. 
+		5'b10111: RESULT = 0;
+		//24. Multiplication
+		5'b11000: RESULT = RES_MUL;
+		//25. 
+		5'b11001: RESULT = 0;
+		//26. 
+		5'b11010: RESULT = 0;
+		//27. 
+		5'b11011: RESULT = 0;
+		//28. DIV DIvition
+		5'b11100: RESULT = RES_DIV;
+		//29. REM Remainder
+		5'b11101: RESULT = RES_REM;
+		//30. 
+		5'b11110: RESULT = 0;
+		//31. 
+		5'b11111: RESULT = 0;
         default: 
             RESULT = 0; 
         endcase
