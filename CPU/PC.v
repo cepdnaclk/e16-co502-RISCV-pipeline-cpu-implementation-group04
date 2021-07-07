5b67032d51c89e5554b1b7f0c804152d8239e7cd
Subhash rathnayake <67975782+subhashnew@users.noreply.github.com>
2021-07-07 18:55:46 +0530

    Update PC.v

--- CPU/PC.v
+++ CPU/PC.v
@@ -1,36 +1,107 @@
-module cpu(PC,INSTRUCTIONS,CLK,RESET);
-	//declaring inputs and outputs
-	input [31:0] BRANCH_PC;
-	input CLK,RESET,CON_BRANCH;
+
+//control unit***************************************************
+module ctrl_unit(OP,FUN3,FUN7,CLK,RESET,MEM_READ,MEM_WRITE,REG_WRITE,MEM_TO_REG,BRANCH,REG_DEST,ALU_SOURCE,ALU_OP,IMMI_SEL,PC_SEL);
+
+	//declareing input 
+	input [6:0] OP,FUN7;
+	input [2:0] FUN3;
+	input CLK,RESET;
+	//declaring output
+	output reg [4:0] ALU_OP;
+	output reg [2:0]  MEM_READ,MEM_WRITE,IMMI_SEL;
+	output reg [1:0] MEM_TO_REG,ALU_SOURCE;
+	output reg REG_WRITE,BRANCH,REG_DEST,PC_SEL;
+
 	
-	output reg [31:0] PC;
-	output reg [31:0] PC_4;
-	//declaring wires
+//r type instructions
+	always @ (OP,FUN3,FUN7) begin
+		if(OP[6:0] == 7'b0110011)  begin  //rtype
+			MEM_READ = 3'b000;
+			MEM_WRITE = 3'b000;
+			REG_WRITE = 1'b1;
+			MEM_TO_REG = 2'b00;
+			BRANCH = 1'b0;
+			ALU_SOURCE = 2'b00;
+			REG_DEST = 1'b1;
+			
+			
+			if(FUN7[6:0] == 7'b0000000)  begin  
+				case (FUN3)
+					8'b000: begin   
+						ALU_OP = 5'b00000; //add
+					end
+					8'b001: begin                            
+						ALU_OP = 5'b00001; // sll
+					end
+					8'b010: begin                            
+						ALU_OP = 5'b00010; // slt
+					end
+					8'b011: begin                            
+						ALU_OP = 5'b00011; // sltu
+					end
+					8'b100: begin                            
+						ALU_OP = 5'b00100; // XOR
+					end
+					8'b101: begin                            
+						ALU_OP = 5'b00101; // SRL
+					end
+					8'b110: begin                            
+						ALU_OP = 5'b00110; // OR
+					end
+					8'b111: begin                            
+						ALU_OP = 5'b00111; // AND
+					end
+					
+				endcase
+			
+			end 
+			else if(FUN7[6:0] == 7'b0100000) begin
+				case (FUN3)
+					8'b000: begin   
+						ALU_OP = 5'b10000; //SUB
+					end
+					8'b101: begin                            
+						ALU_OP = 5'b10101; // SRA
+					end
+
+				endcase
+			
+			
+			end 
+			else if(FUN7[6:0] == 7'b0111011) begin
+				case (FUN3)
+					8'b000: begin   
+						ALU_OP = 5'b11000; //MUL
+					end
+					8'b001: begin                            
+						ALU_OP = 5'b11001; // MULH
+					end
+					8'b010: begin                            
+						ALU_OP = 5'b11010 ;// MULHSU
+					end
+					8'b011: begin                            
+						ALU_OP = 5'b11011;// MULHU
+					end
+					8'b100: begin                            
+						ALU_OP = 5'b11100 ;// DIV
+					end
+					8'b101: begin                            
+						ALU_OP = 5'b11101; // REM
+					end
+					8'b111: begin                            
+						ALU_OP = 5'b11111; // REMU
+					end
+					
+				endcase
+			end
+			
+		
+
+		
+		end	//end of op code		
+					
 	
-	reg [31:0] pc_hold;
-	initial begin   //initial settings
-		pc_hold = 32'b0;//initialy it is zero
-		end
-		
-	always @(posedge CLK,posedge RESET) begin
-		
-		if(RESET == 1'b0  )begin  //normal mood
-			pc_hold = pc_hold + 4; 
-		end
-		
-		if(RESET == 1'b0 && CON_BRANCH == 1'b1) begin
-			pc_hold = pc_hold + BRANCH_PC;
-		
-		end
-		
-		if(RESET == 1'b1) begin //if the reset happen
-		pc_hold = -4; 
-		end
-		
-		#1   //delay to update the pc 
-		PC = pc_hold;
 	end
-	
-
-
+		
+		
 endmodule
