ee41718a3abe9fe7a25002c6edbc81bea04f3cbf
Subhash rathnayake <67975782+subhashnew@users.noreply.github.com>
2021-08-08 16:13:18 +0530

    Add files via upload

---
+++ CPU beta test/adder.v
@@ -0,0 +1,11 @@
+`timescale 1ns/100ps
+
+module adder(INPUT1, INPUT2, RESULT); 
+
+    input [31:0] INPUT1, INPUT2; 
+    output reg [31:0] RESULT;
+    always @(*) begin
+      RESULT = (INPUT1<<2) + INPUT2;
+    end
+
+endmodule
