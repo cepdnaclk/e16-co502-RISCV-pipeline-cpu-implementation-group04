e2b5dcbb39a6a6d79e4f7ddfaf133afb85b39b5b
Vindula Rathnayake <72916960+VindulaNR@users.noreply.github.com>
2021-08-08 14:35:01 +0530

    Update adder.v

--- CPU/OTHER_MODULES/adder.v
+++ CPU/OTHER_MODULES/adder.v
@@ -2,13 +2,10 @@
 
 module adder(INPUT1, INPUT2, RESULT); 
 
-    input [31:0] INPUT1, INPUT2; 
+	input [31:0] INPUT1, INPUT2; 
     output reg [31:0] RESULT;
-  wire [31:0] temp;
-  temp = INPUT1 <<2 ;
-    always @(*) 
-    begin
-      RESULT = temp + INPUT2;
+    always @(*) begin
+      RESULT = (INPUT1<<2) + INPUT2;
     end
 
 endmodule
