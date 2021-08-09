00125bab8307a4eab53720dcca6c61a91e54adbb
Vindula Rathnayake <72916960+VindulaNR@users.noreply.github.com>
2021-08-09 21:03:26 +0530

    Test instruction added

--- CPU/INS_MEMORY/imem.v
+++ CPU/INS_MEMORY/imem.v
@@ -24,17 +24,32 @@ begin
     readaccess = 0;
 
     // Sample program given below. You may hardcode your software program here, or load it from a file:
-    {memory_array[10'd3],  memory_array[10'd2],  memory_array[10'd1],  memory_array[10'd0]}  = 32'b00000000101000000000000010010011; // addi x1 x0 10
-    {memory_array[10'd7],  memory_array[10'd6],  memory_array[10'd5],  memory_array[10'd4]}  = 32'b00000000000100001000000100110011; // add x2 x1 x1
-    {memory_array[10'd11], memory_array[10'd10], memory_array[10'd9],  memory_array[10'd8]}  = 32'b00000000001000001000000110110011; // add x3 x1 x2
-    {memory_array[10'd15], memory_array[10'd14], memory_array[10'd13], memory_array[10'd12]} = 32'b01000000001000011000001000110011; // sub x4 x3 x2
-    {memory_array[10'd19], memory_array[10'd18], memory_array[10'd17], memory_array[10'd16]} = 32'b00000000001000001001001010110011; // sll x5 x1 x2
-    {memory_array[10'd23], memory_array[10'd22], memory_array[10'd21], memory_array[10'd20]} = 32'b00000000010000000010010100100011; // sw x4 0(x0)
-    {memory_array[10'd27], memory_array[10'd26], memory_array[10'd25], memory_array[10'd24]} = 32'b00000000000100010000000100110011; // add x2 x2 x1
-    {memory_array[10'd31], memory_array[10'd30], memory_array[10'd29], memory_array[10'd28]} = 32'b00000000101000000010001010000011; // lw x5 0(x0)
+    {memory_array[10'd3],  memory_array[10'd2],  memory_array[10'd1],  memory_array[10'd0]}  = 32'b00000000101000001000000010010011; 
+    {memory_array[10'd7],  memory_array[10'd6],  memory_array[10'd5],  memory_array[10'd4]}  = 32'b00000000111100000000000100010011; 
+    {memory_array[10'd11], memory_array[10'd10], memory_array[10'd9],  memory_array[10'd8]}  = 32'b00000001010000000110000110010011; 
+    {memory_array[10'd15], memory_array[10'd14], memory_array[10'd13], memory_array[10'd12]} = 32'b00000001100100000000001000010011;
+    {memory_array[10'd19], memory_array[10'd18], memory_array[10'd17], memory_array[10'd16]} = 32'b00000000010100000000001010010011; 
+    {memory_array[10'd23], memory_array[10'd22], memory_array[10'd21], memory_array[10'd20]} = 32'b00000000001000001000001100110011; 
+    {memory_array[10'd27], memory_array[10'd26], memory_array[10'd25], memory_array[10'd24]} = 32'b00000000001100001001001110110011; 
+    {memory_array[10'd31], memory_array[10'd30], memory_array[10'd29], memory_array[10'd28]} = 32'b00000000010000000010000000100011; 
+	{memory_array[10'd35], memory_array[10'd34], memory_array[10'd33], memory_array[10'd32]} = 32'b01110110010100001000001110110011; 
+	{memory_array[10'd39], memory_array[10'd38], memory_array[10'd37], memory_array[10'd36]} = 32'b00000000000000000010001010000011; 
 end
 
-
+/*
+This Instruction set 1 for testing
+No Dta hazzard in this Instruction set there fore no NOPs added
+addi x1 x1 10
+addi x2 x0 15
+ori x3 x0 20
+addi x4 x0 25
+addi x5 x0 5
+add x6 x1 x2
+sll x7 x1 x3
+sw x4 0(x0)
+mul x7 x1 x5
+lw x5 0(x0) 
+*/
 
 //Detecting an incoming memory access
 always @(read)
