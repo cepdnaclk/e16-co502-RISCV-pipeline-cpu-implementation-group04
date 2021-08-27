fdaabf67d958ea26d3d1e0edaa46f6baeb718b55
Vindula Rathnayake <72916960+VindulaNR@users.noreply.github.com>
2021-08-27 23:38:10 +0530

    ALU timing updated

--- CPU Test/ALU/alu.v
+++ CPU Test/ALU/alu.v
@@ -32,51 +32,51 @@ module alu(DATA1, DATA2, RESULT,BRANCH,SELECT);
 	wire BR_BEQ, BR_BNE, BR_BLT, BR_BGE, BR_BLTU, BR_BGEU;
 	//AND,OR,XOR operations with 1 time unit delay
 	//these are bitwise operations
-	assign #2 RES_AND = DATA1 & DATA2;
-	assign #2 RES_OR  = DATA1 | DATA2;
-	assign #2 RES_XOR = DATA1 ^ DATA2;
+	assign #3 RES_AND = DATA1 & DATA2;
+	assign #3 RES_OR  = DATA1 | DATA2;
+	assign #3 RES_XOR = DATA1 ^ DATA2;
 	
 	//forwad operation
-	assign #2 RES_FWD = DATA2;
+	assign #3 RES_FWD = DATA2;
 	
 	//ADD, SUB operations with 2 time unit delay
-	assign #2 RES_ADD = DATA1+DATA2;
-	assign #2 RES_SUB = DATA1-DATA2;
+	assign #3 RES_ADD = DATA1+DATA2;
+	assign #3 RES_SUB = DATA1-DATA2;
 	
 	//shift operations with 1 time unit delay
-	assign #2 RES_SLL = DATA1 << DATA2;
-	assign #2 RES_SRL = DATA1 >> DATA2;
-	assign #2 RES_SRA = DATA1 >>> DATA2;
+	assign #3 RES_SLL = DATA1 << DATA2;
+	assign #3 RES_SRL = DATA1 >> DATA2;
+	assign #3 RES_SRA = DATA1 >>> DATA2;
 
 	//multiplication and divition operations
-	assign #2 RES_MUL = DATA1 * DATA2;
-	assign #2 RES_MULH =  $signed(DATA1) * $signed(DATA2);
-	assign #2 RES_MULHSU = $signed(DATA1) * $unsigned(DATA2);
-	assign #2 RES_MULHU = $unsigned(DATA1) * $unsigned(DATA2);	
-	assign #2 RES_DIV = DATA1 / DATA2;
+	assign #3 RES_MUL = DATA1 * DATA2;
+	assign #3 RES_MULH =  $signed(DATA1) * $signed(DATA2);
+	assign #3 RES_MULHSU = $signed(DATA1) * $unsigned(DATA2);
+	assign #3 RES_MULHU = $unsigned(DATA1) * $unsigned(DATA2);	
+	assign #3 RES_DIV = DATA1 / DATA2;
 	
 	//Remainder operaion
-	assign #2 RES_REM = $signed(DATA1) % $signed(DATA2);
-	assign #2 RES_REMU = $unsigned(DATA1) % $unsigned(DATA2);
+	assign #3 RES_REM = $signed(DATA1) % $signed(DATA2);
+	assign #3 RES_REMU = $unsigned(DATA1) % $unsigned(DATA2);
 	
 	//less than operation
-	assign #2 RES_SLT = ($signed(DATA1) < $signed(DATA2)) ? 1'b1 : 1'b0;
-	assign #2 RES_SLTU = ($unsigned(DATA1) < $unsigned(DATA2)) ? 1'b1 : 1'b0;
+	assign #3 RES_SLT = ($signed(DATA1) < $signed(DATA2)) ? 1'b1 : 1'b0;
+	assign #3 RES_SLTU = ($unsigned(DATA1) < $unsigned(DATA2)) ? 1'b1 : 1'b0;
 	
 	
 	//Branch signals
-	assign #2 BR_BEQ = (DATA1 == DATA2);
-	assign #2 BR_BNE = (DATA1 != DATA2);
-	assign #2 BR_BLT = ($signed(DATA1) < $signed(DATA2));
-	assign #2 BR_BGE = ($signed(DATA1) >= $signed(DATA2));
-	assign #2 BR_BLTU = DATA1 < DATA2;
-	assign #2 BR_BGEU = DATA1 >= DATA2;
+	assign #3 BR_BEQ = (DATA1 == DATA2);
+	assign #3 BR_BNE = (DATA1 != DATA2);
+	assign #3 BR_BLT = ($signed(DATA1) < $signed(DATA2));
+	assign #3 BR_BGE = ($signed(DATA1) >= $signed(DATA2));
+	assign #3 BR_BLTU = DATA1 < DATA2;
+	assign #3 BR_BGEU = DATA1 >= DATA2;
 	
 	
 	//reg BEQ;
 	always @ (DATA1,DATA2,SELECT)		//done it whe there is a change in values and do the operation with 1 time interval delay
 	//2 time unit redelay added becuase the is worse 2 time unit delay from pipeline to alu
-	begin #2
+	begin #3
     	case(SELECT)
 
     	//0. ADD operation 
